0c4bbb492ddadfb54e46ba42f0a2eeddeba2efc9
Jan Hlavacek <jhlavace@svsu.edu>
2024-10-20 18:58:13 -0400

    Make it a simple package that can be installed

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='canvas',
+    version='0.1',
+    description='Package for for interacting with Canvas LMS',
+    url='https://github.com/lahvak/canvas_scripts',
+    author='Jan Hlavacek',
+    author_email='jhlavace@svsu.edu',
+    license='Apache 2.0',
+    packages=['canvas'],
+    zip_safe=False,
+)
